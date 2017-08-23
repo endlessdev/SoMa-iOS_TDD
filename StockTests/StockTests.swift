b5544dd002f0f6ffa7df9ebf4255a0448d4ba3a0
Jade Yeom <ysw0094@gmail.com>
2017-08-23 15:18:13 +0900

    test: write test for stock model

--- StockTests/StockTests.swift
+++ StockTests/StockTests.swift
@@ -9,28 +9,21 @@
 import XCTest
 @testable import Stock
 
-class StockTests: XCTestCase {
+final class StockTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testStockNotNil(){
+        let stock = Stock()
+        XCTAssertNotNil(stock)
     }
     
 }
